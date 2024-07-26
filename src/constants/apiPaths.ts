713432a477a1fe4ad5566e6cf0d533ac28b18834
Ksenia Pilshchikova <ksushapi@gmail.com>
2024-07-26 17:15:27 +0200

    feat: update cart queries

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://7mimugfx16.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://pf6r872y4h.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://871xbpugrg.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://7mimugfx16.execute-api.eu-west-1.amazonaws.com/dev",
   new: "",
 };
 
