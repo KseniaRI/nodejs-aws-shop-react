0b35a4466beb59ecb115184b7de3d74bf8964f90
Ksenia Pilshchikova <ksushapi@gmail.com>
2024-08-03 11:36:12 +0200

    feat: update link to https

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://7mimugfx16.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://871xbpugrg.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://c60y43de2i.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://pf6r872y4h.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://871xbpugrg.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://7mimugfx16.execute-api.eu-west-1.amazonaws.com/dev",
-  new: "",
+  cart: "https://c60y43de2i.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
